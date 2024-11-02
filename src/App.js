9d2f508c6ed0f50eab927982b63c99ca04c4080b
Ayush <ayushg1500@gmail.com>
2024-11-02 11:44:01 +0530

    Initial commmit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useState } from "react";
+
 function App() {
+  let [text, setText] = useState(null);
+  function change() {
+    if (text == null) {
+      text = "Welcome To React";
+      setText(text);
+    } else {
+      text = null;
+      setText(text);
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App ">
+      <button id="btn" onClick={() => change()}>
+        Show/Hide
+      </button>
+      <p id="para">{text}</p>
     </div>
   );
 }
 
 export default App;
+
